d3abbdb4dc89f46e89be0dc1f1f2b6d0fefb5545
Juan Pablo <jpriveram01@gmail.com>
2025-08-21 09:46:29 -0400

    feat: Saluda por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,14 +1,16 @@
 import { construirSaludo } from "./saludador";
 
 const nombre_input = document.querySelector("#nombre");
+const genero_input= document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const nombre = (nombre_input.value || "").trim();
-  const mensaje = construirSaludo(nombre);
+  const nombre = (nombre_input.value);
+  const genero = (genero_input.value);
+  const mensaje = construirSaludo(nombre,genero);
 
   div.innerHTML = `<p>${mensaje}</p>`;
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,12 +1,13 @@
-export function construirSaludo(nombre,  fecha = new Date()) {
+export function construirSaludo(nombre,genero, fecha = new Date()) {
     const hora = fecha.getHours();
     let saludo = "Hola";
     
     if (hora < 12) saludo = "Buenos días";
     else if (hora < 19) saludo = "Buenas tardes";
     else saludo = "Buenas noches";
-  
-   
+
+    if (genero === "femenino") saludo += " señora";
+    else if (genero === "masculino") saludo += " señor";  
     
   
     return `${saludo} ${nombre}`;
